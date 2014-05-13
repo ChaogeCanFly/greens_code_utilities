6c95b639d63a4b0049cbf779e1736c014470c827
jdoppler <joerg.doppler@tuwien.ac.at>
2014-05-13 08:36:27 +0200

    now only positional arguments + minor changes

--- show_Smat_diff.py
+++ show_Smat_diff.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python2.7
 
 import argh
+import numpy as np
 
-@argh_hack()
-def show_Smat_diff(folder_1=".", folder_2=".", datafile="Smat.sine_boundary.dat"):
-    multi = np.loadtxt("{}/{}".format(folder_1, datafile), unpack=True, skiprows=4)
-    single = np.loadtxt("{}/{}".format(folder_2, datafile), unpack=True, skiprows=4)
+@argh.arg_hack()
+def show_Smat_diff(a, b):
+    """Print difference between input files a and b."""
+
+    params = {'unpack': True, 
+              'skiprows': 4}
+
+    a, b = [ np.loadtxt(i, **params) for i in (a,b) ]
+
+    print "File a:" 
+    print a
+    print
+    print "File b:"
+    print b
+    print
+    print "Difference (a - b):"
+    print a - b
 
 if __name__ == '__main__':
-    argh.dispatch_command(show_Smat_diff)
+    argh.dispatch_command(show_Smat_diff, completion=False)
