7dcb448ae98b2ddf32646326e8ae5a6a6889fb15
jdoppler <joerg.doppler@tuwien.ac.at>
2014-07-24 17:05:52 +0200

    initial commit

---
+++ render.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python2.7
+
+import argh
+import subprocess
+
+@argh.arg("-p", "--ppm", required=True)
+@argh.arg("-j", "--jpg", required=True)
+def povray(ppm="in.ppm", jpg="in.jpg", scriptfile="scene.pov",
+           outfile="out.png", width=4800, height=2250):
+    """Write .pov script and render scene based on the input .ppm and .jpeg files.
+        
+        Parameters:
+        -----------
+            ppm: str
+            jpg: str
+            outfile: str
+            width: int
+            height: int
+    """
+
+    scene = """
+        #include "colors.inc"
+        #include "textures.inc"
+
+        #declare PPM = "%s"
+        #declare JPEG = "%s"
+
+        #declare zoom=10000000;
+        #declare field_height=1;
+
+        #declare ambient_value = 0.7;
+        #declare diffuse_value = 0.1;
+        #declare transmit_value = 0.0;
+        #declare light_height = 10;
+
+        #declare cc=<0,1,0>;
+
+        background {color White}
+
+        camera {
+          orthographic
+          location <-2.5*zoom,5*zoom,7*zoom>
+          right cc*image_width/image_height
+          direction <2.5*zoom,-9*zoom,-7*zoom>
+          angle 0.00001
+          look_at <0, 0,0>
+        }
+
+        height_field {
+            ppm PPM
+            smooth
+            water_level -10.0
+            texture {
+                pigment{
+                    image_map { 
+                        jpeg JPEG
+                        transmit all transmit_value
+                        }
+                rotate x*90
+                }
+                finish {
+                    ambient ambient_value
+                    diffuse diffuse_value
+                    metallic
+                }
+            }
+            scale <14,field_height,-2>
+            translate<-7,0,1>
+            no_shadow
+            }
+
+        light_source {
+            <0,light_height,0>
+            color White
+            parallel
+            point_at<0,0,0>
+            media_interaction 1
+            media_attenuation 1
+            fade_distance 1
+        }  
+    """ % (ppm, jpg)
+
+    with open(scriptfile, "w") as f:
+        f.write(scene)
+
+    params = {  'W': width,
+                'H': height,
+                'I': scriptfile,
+                'O': outfile
+                }
+    cmd = "povray -W{W} -H{H} -I{I} -D -O{O}".format(**params)
+    subprocess.check_call(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    argh.dispatch_command(povray)
+
+  
+
+  
