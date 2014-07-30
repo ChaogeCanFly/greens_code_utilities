517bee0311756b1f347cee4625ab03956f9e40df
jdoppler <joerg.doppler@tuwien.ac.at>
2014-07-30 15:01:50 +0200

    minor changes

--- jordan.py
+++ jordan.py
@@ -6,9 +6,10 @@ from ep.waveguide import Waveguide
 import numpy as np
 import re
 
+
 class Jordan(object):
     """Find the Jordan-block of a multiple eigenvalue problem.
-    
+
         Parameters:
         -----------
             x0, y0: float
@@ -24,7 +25,7 @@ class Jordan(object):
             evalsfile: str
                 Eigenvalue file.
             xml: str
-               Input .xml file name. 
+               Input .xml file name.
             kwargs:
                 Parameters of the ep.waveguide.Waveguide class.
 
@@ -39,9 +40,9 @@ class Jordan(object):
             dx: float
                 Grid discretization.
     """
-    
-    def __init__(self, x0, y0, dx=1e-2, dy=1e-2, rtol=1e-6, 
-                 executable='solve_xml_mumps', datafile='jordan.out', 
+
+    def __init__(self, x0, y0, dx=1e-2, dy=1e-2, rtol=1e-6,
+                 executable='solve_xml_mumps', datafile='jordan.out',
                  evalsfile='Evals.sine_boundary.dat',
                  xml='input.xml', **kwargs):
 
@@ -52,6 +53,7 @@ class Jordan(object):
         self.dx = None
 
         self.executable = executable
+        self.datafile = datafile
         self.xml = xml
         self.evalsfile = evalsfile
 
@@ -71,14 +73,14 @@ class Jordan(object):
                     pph = re.split("[><]", line)[-3]
                     dx = 1./(float(pph) + 1)
         self.dx = dx
-        
+
     @classmethod
     def _get_eigenvalues(self, evalsfile=None, dx=None):
         if not evalsfile:
             evalsfile = self.evalsfile
         if not dx:
             dx = self.dx
-        beta = np.genfromtxt(evalsfile, unpack=True, dtype=complex, usecols=(0),
+        beta = np.genfromtxt(evalsfile, unpack=True, dtype=complex, usecols=0,
                              converters={0: lambda s: convert_to_complex(s)})
         k = np.angle(beta)/dx
         k_l = k[:len(k)/2]
@@ -90,7 +92,7 @@ class Jordan(object):
         (x0, y0), (x1, y1) = self.values[-2:]
         dx, dy = x1-x0, y1-y0
 
-        parameters = [ (n,m) for n in x0, x1 for m in y0, y1 ]
+        parameters = [ (n,m) for n in x0, x1 for m in y0,y1 ]
         for x, y in parameters:
             self._run_code()
             e00, e01, e10, e11 = self._get_eigenvalues(x,y)
@@ -100,7 +102,7 @@ class Jordan(object):
         vx /= dx
         vy = (e01[0]-e01[1]) - (e00[0]-e00[1])
         vy /= dy
-        
+
         norm = np.sqrt(vx**2 + vy**2)
         res_x = vx/norm * (e00[0] - e00[1])
         res_y = vy/norm * (e00[0] - e00[1])
@@ -125,30 +127,30 @@ class Jordan(object):
             self._update_boundary(xi, yi)
 
         print self.values
-        np.savetxt(datafile, self.values)
+        np.savetxt(self.datafile, self.values)
 
 
 def convert_to_complex(s):
     """Convert a string of the form (x,y) to a complex number z = x+1j*y.
-        
+
         Parameters:
         -----------
             s: str
-            
+
         Returns:
         -------
             z: complex
     """
 
     regex = re.compile(r'\(([^,\)]+),([^,\)]+)\)')
-    x, y =  map(float, regex.match(s).groups())
+    x, y = map(float, regex.match(s).groups())
     return x + 1j*y
-    
 
-#def find_EP(*args, **kwargs**):
-#    J = Jordan(*args, **kwargs)
-#    J.solve()
-#            
+
+def find_EP(*args, **kwargs):
+    J = Jordan(*args, **kwargs)
+    J.solve()
+
+
 if __name__ == '__main__':
-    argh.dispatch_command(jordan)
-    
+    argh.dispatch_command(find_EP)
