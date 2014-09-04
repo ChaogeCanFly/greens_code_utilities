14993d9378d4fc622a9cceb49199c00bed7b01b9
jdoppler <joerg.doppler@tuwien.ac.at>
2014-09-04 17:41:53 +0200

    added a wrapper for np.loadtxt which correctly handles negative imaginary parts

--- helpers.py
+++ helpers.py
@@ -4,6 +4,9 @@
     convert_to_complex(s):
         Convert a string of the form (x,y) to a complex number z = x+1j*y.
 
+    loadtxt_complex(filename, **loadtxt_kwargs):
+        Wrapper for numpy's loadtxt which replaces all '+-' with '-' beforehand.
+
     natural_sorting(text, args="delta", sep="_")
         Sort a text with respect to a given argument value.
 
@@ -11,6 +14,7 @@
         Replace some lines in an input file and write to output file.
         The replacements are supplied via an dictionary.
 """
+import numpy as np
 import os
 import re
 
@@ -20,7 +24,18 @@ def convert_to_complex(s):
 
     regex = re.compile(r'\(([^,\)]+),([^,\)]+)\)')
     x, y = map(float, regex.match(s).groups())
+
     return x + 1j*y
+
+
+def loadtxt_complex(filename, **loadtxt_kwargs):
+    """Wrapper for numpy's loadtxt which replaces all '+-' with '-' beforehand."""
+
+    with open(filename, "r") as f:
+        lines = map(lambda x: x.replace("+-", "-"), f)
+        array = np.loadtxt(lines, dtype=np.complex128, **loadtxt_kwargs)
+
+    return array
 
 
 def natural_sorting(text, args="delta", sep="_"):
