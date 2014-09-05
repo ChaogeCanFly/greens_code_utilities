69beb4a80509eed815fb9131770357f1c4f5482d
jdoppler <joerg.doppler@tuwien.ac.at>
2014-09-05 09:54:51 +0200

    added full option & minor changes

--- plot_3D_spectrum.py
+++ plot_3D_spectrum.py
@@ -58,7 +58,7 @@ def reorder_file(infile="bloch.tmp", outfile="bloch_reordered.tmp"):
 @argh.arg("-l", "--limits", type=float, nargs="+")
 def plot_3D_spectrum(infile="bloch.tmp", outfile="bloch_reordered.tmp",
                      reorder=False, jump=100., mayavi=False, limits=None,
-                     girtsch=False, sort=False, png=None):
+                     girtsch=False, sort=False, png=None, full=False):
     """Visualize the eigenvalue spectrum with mayavi.mlab's mesh (3D) and
     matplotlib's pcolormesh (2D).
 
@@ -81,6 +81,10 @@ def plot_3D_spectrum(infile="bloch.tmp", outfile="bloch_reordered.tmp",
                 Whether to account for a wrong eps <-> delta labeling.
             sort: bool
                 Sorts the eigenvalues such that one is larger than the other.
+            png: str
+                Save the heatmap plots in a .png file.
+            full: bool
+                Add additional heatmap plots.
     """
     if reorder:
         print "reordering..."
@@ -105,12 +109,12 @@ def plot_3D_spectrum(infile="bloch.tmp", outfile="bloch_reordered.tmp",
         tmp0, tmp1 = 1.*ev0, 1.*ev1
         tmp0[ev1 > ev0] = ev1[ev1 > ev0]
         tmp1[ev1 > ev0] = ev0[ev1 > ev0]
-        
+
         ev0, ev1 = 1.*tmp0, 1.*tmp1
 
     # get eps/delta meshgrid
-    eps, delta, ev0, ev1 = [ x.reshape(len_eps, len_delta) for x in
-                                                        eps, delta, ev0, ev1 ]
+    eps, delta, ev0, ev1 = [ x.reshape(len_eps, len_delta) for
+                              x in eps, delta, ev0, ev1 ]
 
     # set x/y limits
     if limits:
@@ -126,18 +130,14 @@ def plot_3D_spectrum(infile="bloch.tmp", outfile="bloch_reordered.tmp",
     ev1 = np.ma.masked_where(np.isnan(ev1), ev1)
 
     # print minimum of eigenvalue difference
-    i, j = np.unravel_index(abs(ev0.real-ev1.real).argmin(), ev0.shape)
-    print "Real part:"
-    print "eps_min =", eps[i,j]
-    print "delta_min =", delta[i,j]
-    i, j = np.unravel_index(abs(ev0.imag-ev1.imag).argmin(), ev0.shape)
-    print "Imaginary part:"
-    print "eps_min =", eps[i,j]
-    print "delta_min =", delta[i,j]
+    i, j = np.unravel_index(np.sqrt((ev0.real-ev1.real)**2 +
+                                    (ev0.imag-ev1.imag)**2).argmin(), ev0.shape)
+    print "Approximate EP location:"
+    print "eps_EP =", eps[i,j]
+    print "delta_EP =", delta[i,j]
 
     if mayavi:
         # real part
-        # mask = np.zeros_like(eps).astype(bool)
         for e in ev0, ev1:
             mask = np.zeros_like(eps).astype(bool)
             mask[np.abs(np.diff(e.real, axis=0)) > jump] = True
@@ -163,36 +163,68 @@ def plot_3D_spectrum(infile="bloch.tmp", outfile="bloch_reordered.tmp",
         mlab.axes(color=(0,0,0), nb_labels=3, xlabel="epsilon", ylabel="delta",
                   zlabel="Im(K)")
         mlab.show()
-    else:
-        f, axes = plt.subplots(nrows=3, ncols=2, sharex=True, sharey=True)
-        (ax1, ax2), (ax3, ax4), (ax5, ax6) = axes
+    elif full:
+        f, axes = plt.subplots(nrows=4, ncols=2, sharex=True, sharey=True)
+        (ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8) = axes
 
         plt.xticks(rotation=70)
         plt.suptitle(infile)
 
-        ax1.set_title("ev0.real")
+        ax1.set_title(r"$\Re K_0$")
         im1 = ax1.pcolormesh(eps, delta, ev0.real, cmap=plt.get_cmap('coolwarm'))
-        ax2.set_title("ev0.imag")
+        ax2.set_title(r"$\Im K_0$")
         im2 = ax2.pcolormesh(eps, delta, ev0.imag, cmap=plt.get_cmap('coolwarm'))
-        ax3.set_title("ev1.real")
+        ax3.set_title(r"$\Re K_1$")
         im3 = ax3.pcolormesh(eps, delta, ev1.real, cmap=plt.get_cmap('coolwarm'))
-        ax4.set_title("ev1.imag")
+        ax4.set_title(r"$\Im K_1$")
         im4 = ax4.pcolormesh(eps, delta, ev1.imag, cmap=plt.get_cmap('coolwarm'))
-        
-        ax5.set_title(r"$|$ev1.real - ev0.real$|$")
-        Z = abs(ev1.real - ev0.real)
-        im5 = ax5.pcolormesh(eps, delta, Z, cmap=plt.get_cmap('gray'), vmin=0)
-        ax6.set_title(r"$|$ev1.imag - ev0.imag$|$")
-        Z = abs(ev1.imag - ev0.imag)
-        im6 = ax6.pcolormesh(eps, delta, Z, cmap=plt.get_cmap('gray'), vmin=0)
-        for im, ax in zip((im1, im2, im3, im4, im5, im6), 
-                          (ax1, ax2, ax3, ax4, ax5, ax6)):
+
+        ax5.set_title(r"$|\Re K_0 - \Re K_1|^2$")
+        Z_real = abs(ev1.real - ev0.real)
+        im5 = ax5.pcolormesh(eps, delta, Z_real, cmap=plt.get_cmap('gray'), vmin=0)
+        ax6.set_title(r"$|\Im K_0 - \Im K_1|^2$")
+        Z_imag = abs(ev1.imag - ev0.imag)
+        im6 = ax6.pcolormesh(eps, delta, Z_imag, cmap=plt.get_cmap('gray'), vmin=0)
+
+        Z = np.sqrt(Z_imag**2 + Z_real**2)
+        ax7.set_title(r"$\sqrt{(\Re K_0 - \Re K_1)^2 + (\Im K_0 - \Im K_1)^2}$")
+        im7 = ax7.pcolormesh(eps, delta, Z, cmap=plt.get_cmap('gray'), vmin=0)
+
+        for im, ax in zip((im1, im2, im3, im4, im5, im6, im7),
+                          (ax1, ax2, ax3, ax4, ax5, ax6, ax7)):
+            print im, ax
             ax.set_xlabel("epsilon")
             ax.set_ylabel("delta")
             ax.set_xlim(eps.min(), eps.max())
             ax.set_ylim(delta.min(), delta.max())
             f.colorbar(im, ax=ax)
             plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+
+        plt.tight_layout()
+        if png:
+            plt.savefig(png)
+        else:
+            plt.show()
+    else:
+        f, ax = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
+
+        plt.xticks(rotation=70)
+        plt.suptitle(infile)
+
+        Z_real = abs(ev1.real - ev0.real)
+        Z_imag = abs(ev1.imag - ev0.imag)
+        Z = np.sqrt(Z_imag**2 + Z_real**2)
+
+        ax.set_title(r"$\sqrt{(\Re K_0 - \Re K_1)^2 + (\Im K_0 - \Im K_1)^2}$")
+        im = ax.pcolormesh(eps, delta, Z, cmap=plt.get_cmap('gray'), vmin=0)
+
+        ax.set_xlabel("epsilon")
+        ax.set_ylabel("delta")
+        ax.set_xlim(eps.min(), eps.max())
+        ax.set_ylim(delta.min(), delta.max())
+        f.colorbar(im, ax=ax)
+        plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+
         plt.tight_layout()
         if png:
             plt.savefig(png)
