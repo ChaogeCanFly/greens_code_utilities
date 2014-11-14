f0cae427da224c6be6d06e8dc134663684661f44
jdoppler <joerg.doppler@tuwien.ac.at>
2014-11-14 11:02:47 +0100

    minor changes in docstrings

--- helpers.py
+++ helpers.py
@@ -5,7 +5,8 @@
         Convert a string of the form (x,y) to a complex number z = x+1j*y.
 
     loadtxt_complex(filename, **loadtxt_kwargs):
-        Wrapper for numpy's loadtxt which replaces all '+-' with '-' beforehand.
+        Wrapper for numpy's loadtxt which replaces all '+-' with '-' before
+        evaluation.
 
     natural_sorting(text, args="delta", sep="_")
         Sort a text with respect to a given argument value.
@@ -34,7 +35,8 @@ def convert_to_complex(s):
 
 
 def loadtxt_complex(filename, **loadtxt_kwargs):
-    """Wrapper for numpy's loadtxt which replaces all '+-' with '-' beforehand."""
+    """Wrapper for numpy's loadtxt which replaces all '+-' with '-' before
+    evaluation."""
 
     with open(filename, "r") as f:
         lines = map(lambda x: x.replace("+-", "-"), f)
@@ -71,14 +73,17 @@ def replace_in_file(infile, outfile, **replacements):
 def get_git_log(lines=5, relative_git_path="", outfile=None):
     """Return the 'git log' output of the calling file."""
 
-    path = os.path.dirname(os.path.realpath(sys.argv[0]))
-    gitpath = os.path.join(path, relative_git_path, ".git")
-    cmd = "git --git-dir {} log".format(gitpath)
-    gitlog = subprocess.check_output(cmd.split())
-    gitlog_abbrev = gitlog.splitlines()[:lines+1]
+    try:
+        path = os.path.dirname(os.path.realpath(sys.argv[0]))
+        gitpath = os.path.join(path, relative_git_path, ".git")
+        cmd = "git --git-dir {} log".format(gitpath)
+        gitlog = subprocess.check_output(cmd.split())
+        gitlog_abbrev = gitlog.splitlines()[:lines+1]
 
-    if outfile:
-        with open(outfile, "w") as f:
-            f.write("\n".join(gitlog_abbrev))
-    else:
-        return " ".join(gitlog_abbrev)
+        if outfile:
+            with open(outfile, "w") as f:
+                f.write("\n".join(gitlog_abbrev))
+        else:
+            return " ".join(gitlog_abbrev)
+    except:
+        sys.exit("No .git directory found.")
