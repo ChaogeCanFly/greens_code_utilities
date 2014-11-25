c353c810f8b6865cb26b8b9061957f4bb1129b10
jdoppler <joerg.doppler@tuwien.ac.at>
2014-11-25 18:55:49 +0100

    added docstring; minor changes

--- get_loop_eigenfunctions.py
+++ get_loop_eigenfunctions.py
@@ -6,6 +6,7 @@ import numpy as np
 import os
 import shutil
 import subprocess
+import sys
 
 import argh
 
@@ -17,12 +18,14 @@ import helpers
 
 def get_loop_eigenfunction(N=1.05, eta=0.0, L=5., init_phase=-0.05, eps=0.05,
                            loop_direction="+", loop_type='Bell', pphw=100):
+    """Return the instantaneous eigenfunctions and eigenvectors for each step
+    in a parameter space loop."""
 
     # get input.xml
     greens_path = os.environ.get('GREENS_CODE_XML')
     XML = os.path.join(greens_path, "input_periodic_cell.xml")
 
-    x = np.linspace(0, L, 20)
+    x = np.linspace(0, L, 100)
     y = np.linspace(0, 1., (pphw*N+1))
 
     wg_kwargs = {'N': N,
@@ -33,17 +36,18 @@ def get_loop_eigenfunction(N=1.05, eta=0.0, L=5., init_phase=-0.05, eps=0.05,
                  'loop_type': loop_type}
     WG = Waveguide(**wg_kwargs)
     WG.x_EP = eps
+    WG.solve_ODE()
+
     eps, delta = WG.get_cycle_parameters(x)
 
     Bloch_data = []
-    Bloch_vector_0 = []
-    Bloch_vector_1 = []
+    K_0, K_1, Chi_0, Chi_1 = [ [] for n in range(4) ]
     for n, (xn, epsn, deltan) in enumerate(zip(x, eps, delta)):
-        print "xn, n", xn, n
+
+        ID = "n_{}_xn_{:08.4f}".format(n, xn)
+        print ID
 
         # prepare waveguide and profile
-        XML = os.path.join(greens_path, XML)
-
         profile_kwargs = {'eps': epsn,
                           'delta': deltan,
                           'pphw': pphw,
@@ -53,7 +57,7 @@ def get_loop_eigenfunction(N=1.05, eta=0.0, L=5., init_phase=-0.05, eps=0.05,
         wg_kwargs_n = {'N': N,
                        'eta': eta,
                        'L': 2*np.pi/(WG.kr + deltan),
-                       # 'init_phase': init_phase,
+                       'init_phase': 0.0*init_phase,
                        'loop_direction': loop_direction,
                        'loop_type': 'Constant'}
 
@@ -61,7 +65,8 @@ def get_loop_eigenfunction(N=1.05, eta=0.0, L=5., init_phase=-0.05, eps=0.05,
         ep.profile.Generate_Profiles(**profile_kwargs)
 
         # run code
-        cmd = "mpirun -np 4 solve_xml_mumps dev"
+        # cmd = "mpirun -np 4 solve_xml_mumps dev"
+        cmd = "solve_xml_mumps dev"
         greens_code = subprocess.Popen(cmd.split(),
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE)
@@ -69,72 +74,96 @@ def get_loop_eigenfunction(N=1.05, eta=0.0, L=5., init_phase=-0.05, eps=0.05,
 
         for file in glob.glob("N_*profile"):
             if "lower" in file:
-                shutil.move(file, "xn_{:08.4f}.lower_profile".format(xn))
+                shutil.move(file, ID + ".lower_profile")
             if "upper" in file:
-                shutil.move(file, "xn_{:08.4f}.upper_profile".format(xn))
+                shutil.move(file, ID + ".upper_profile")
 
-        shutil.move("upper.dat", "xn_{:08.4f}.upper_dat".format(xn))
-        shutil.move("lower.dat", "xn_{:08.4f}.lower_dat".format(xn))
+        shutil.move("upper.dat", ID + ".upper_dat")
+        shutil.move("lower.dat", ID + ".lower_dat")
 
         # get Bloch eigensystem
-        K, _, ev, _ = bloch.get_eigensystem(return_eigenvectors=True, verbose=False)
+        K, _, ev, _, v, _ = bloch.get_eigensystem(return_eigenvectors=True,
+                                                  return_velocities=True,
+                                                  verbose=False)
+        if v[0] < 0. or v[1] < 0.:
+            sys.exit("Error: group velocities are negative!")
+
         K0, K1 = K[0], K[1]
         ev0, ev1 = ev[0, :], ev[1, :]
 
         diff_0 = np.abs(np.diff(ev0)).sum()
         diff_1 = np.abs(np.diff(ev1)).sum()
-        print 
         print 'diff before', diff_0
         print 'diff before', diff_1
-        print 
-        print L/2. + init_phase/(WG.kr + deltan)
-        # if diff_0 > diff_1 and xn < L/2.:
-        if K0 > K1 and xn < L/2.:
+        if diff_0 > diff_1 and xn < L/2.:
             ev0, ev1 = ev1, ev0
             K0, K1 = K1, K0
-        # elif diff_0 < diff_1 and xn > L/2.:
-        elif K0 < K1 and xn > L/2.:
+        elif diff_0 < diff_1 and xn > L/2.:
             ev0, ev1 = ev1, ev0
             K0, K1 = K1, K0
-
 
         diff_0 = np.abs(np.diff(ev0)).sum()
         diff_1 = np.abs(np.diff(ev1)).sum()
-        print 
         print 'diff after', diff_0
         print 'diff after', diff_1
-        print 
 
         Bloch_data.append([xn, epsn, deltan,
                            K0, K1, ev0, ev1])
-        Bloch_vector_0.append(ev0)
-        Bloch_vector_1.append(ev1)
-        shutil.copyfile("pic.geometry.sine_boundary.1.jpg",
-                        "xn_{:08.4f}.jpg".format(xn))
-        print xn, epsn, deltan, K0, K1
+        K_0.append(K0)
+        K_1.append(K1)
+        Chi_0.append(ev0)
+        Chi_1.append(ev1)
 
         z = ev0.view(dtype=float)
-        np.savetxt("xn_{:08.4f}.dat".format(xn), zip(y,
-                                                     ev0.real,
-                                                     ev0.imag,
-                                                     np.ones_like(z)*K0.real,
-                                                     np.ones_like(z)*K0.imag,
-                                                     ev1.real,
-                                                     ev1.imag,
-                                                     np.ones_like(z)*K1.real,
-                                                     np.ones_like(z)*K1.imag),
-                   header='y Re(ev0) Im(ev0) Re(K0) Im(K0) Re(ev1) Im(ev1) Re(K1) Im(K1)')
+        np.savetxt(ID + ".dat", zip(y, ev0.real, ev0.imag,
+                                    np.ones_like(z)*K0.real,
+                                    np.ones_like(z)*K0.imag,
+                                    ev1.real, ev1.imag,
+                                    np.ones_like(z)*K1.real,
+                                    np.ones_like(z)*K1.imag),
+                   header=('y Re(ev0) Im(ev0) Re(K0) Im(K0) Re(ev1)'
+                           'Im(ev1) Re(K1) Im(K1)'))
+
+        shutil.copyfile("pic.geometry.sine_boundary.1.jpg", ID + ".jpg")
+
+        print "xn", xn, "epsn", epsn, "deltan", deltan, "K0", K0, "K1", K1
+
+    # full system
+    # Chi_0, Chi_1, K_0, K_1 = [ np.array(z).T for z in Chi_0, Chi_1, K_0, K_1]
+    Chi_0 = np.array(Chi_0).T
+    Chi_1 = np.array(Chi_1).T
 
     X, Y = np.meshgrid(x, y)
+    plt.clf()
+    # plt.pcolormesh(np.abs(Chi_0))
+    plt.pcolormesh(X, Y, np.real(Chi_0 * np.exp(1j*K_0*xn)))
+    plt.savefig("Chi_0.png")
+    plt.clf()
+    # plt.pcolormesh(np.abs(Chi_1))
+    plt.pcolormesh(X, Y, np.real(Chi_1 * np.exp(1j*K_1*xn)))
+    plt.savefig("Chi_1.png")
 
-    Chi_0 = np.array(Bloch_vector_0).T
-    Chi_1 = np.array(Bloch_vector_1).T
+    # effective model predictons
+    Chi_0_eff, Chi_1_eff = WG.eVecs_r[:,:,0], WG.eVecs_r[:,:,1]
+    K_0_eff, K_1_eff = WG.eVals[:,0], WG.eVals[:,1]
 
-    plt.pcolormesh(X, Y, np.real(Chi_0 * np.exp(1j*K0*xn)))
-    plt.savefig("test_0.png")
+    Chi_0_eff_0 = np.outer(Chi_0_eff[:,0], np.ones_like(y))
+    Chi_0_eff_1 = np.outer(Chi_0_eff[:,1]*np.exp(-1j*WG.kr*WG.t),
+                           np.sqrt(2.*WG.k0/WG.k1)*np.cos(np.pi*y))
+    Chi_0_eff = Chi_0_eff_0 + Chi_0_eff_1
 
-    plt.pcolormesh(X, Y, np.real(Chi_1 * np.exp(1j*K1*xn)))
-    plt.savefig("test_1.png")
+    Chi_1_eff_0 = np.outer(Chi_1_eff[:,0], np.ones_like(y))
+    Chi_1_eff_1 = np.outer(Chi_1_eff[:,1]*np.exp(-1j*WG.kr*WG.t),
+                           np.sqrt(2.*WG.k0/WG.k1)*np.cos(np.pi*y))
+    Chi_1_eff = Chi_1_eff_0 + Chi_1_eff_1
+
+    X_eff, Y_eff = np.meshgrid(WG.t, y)
+    plt.clf()
+    plt.pcolormesh(X_eff, Y_eff, np.real(Chi_0_eff.T * np.exp(1j*K_0_eff*xn)))
+    plt.savefig("Chi_0_eff.png")
+    plt.clf()
+    plt.pcolormesh(X_eff, Y_eff, np.real(Chi_1_eff.T * np.exp(1j*K_1_eff*xn)))
+    plt.savefig("Chi_1_eff.png")
 
 
 if __name__ == '__main__':
