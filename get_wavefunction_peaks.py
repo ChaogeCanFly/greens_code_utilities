a199fd8db149e22d0ece08c2c133e922da8578f8
jdoppler <joerg.doppler@tuwien.ac.at>
2015-03-03 13:41:11 +0100

    use *purewavefunction.ascii files to extract data; minor changes

--- get_wavefunction_peaks.py
+++ get_wavefunction_peaks.py
@@ -11,16 +11,14 @@ from ep.potential import gauss
 from helper_functions import convert_to_complex
 
 
-def get_array(input, L=100, W=1, r_nx=None, r_ny=None, abs=True):
+def get_array(input, L=100, W=1, r_nx=None, r_ny=None):
     n, psi = np.genfromtxt(input, unpack=True, usecols=(0,1), dtype=complex,
                            converters={1: convert_to_complex})
     x = np.linspace(0, L, r_nx)
     y = np.linspace(0, W, r_ny)
     X, Y = np.meshgrid(x,y)
-    Z = [ x.reshape(r_ny, r_nx, order='F') for x in psi, ]
-
-    if abs:
-        Z = abs(Z)**2
+    Z = psi.reshape(r_ny, r_nx, order='F')
+    Z = np.abs(Z)**2
 
     return X, Y, Z
 
@@ -34,12 +32,14 @@ def main(pphw=50, N=2.5, L=100, W=1, eps=0.1, sigma=0.01, plot=True):
     array_kwargs = {'L': L,
                     'W': W,
                     'r_nx': r_nx_pot,
-                    'r_ny': r_ny_pot,
-                    'eps': eps}
-    X, Y, Za = get_array(glob("pic.*.0000.streu.*.ascii")[0], **array_kwargs)
-    _, _, Zb = get_array(glob("pic.*.0001.streu.*.ascii")[0], **array_kwargs)
+                    'r_ny': r_ny_pot}
+
+    X, Y, Za = get_array(glob("*.0000.streu.*.purewavefunction.ascii")[0], **array_kwargs)
+    _, _, Zb = get_array(glob("*.0001.streu.*.purewavefunction.ascii")[0], **array_kwargs)
 
     peaks_a, peaks_b = [ get_local_peaks(z, peak_type='minimum') for z in Za, Zb ]
+    peaks_a[np.logical_or(Y > 0.9, Y < 0.1)] = 0.0
+    peaks_b[np.logical_or(Y > 0.9, Y < 0.1)] = 0.0
     idx_a, idx_b = [ np.where(p) for p in peaks_a, peaks_b ]
 
     if plot:
@@ -48,10 +48,10 @@ def main(pphw=50, N=2.5, L=100, W=1, eps=0.1, sigma=0.01, plot=True):
         cmap.set_bad('dimgrey', 1)
 
         scale = 5.
-        ax1.pcolormesh(X, Y, scale*Zma, cmap=cmap)
+        ax1.pcolormesh(X, Y, scale*Za, cmap=cmap)
         ax1.scatter(X[idx_b], Y[idx_b], s=1.5e4, c="w", edgecolors=None)
 
-        ax2.pcolormesh(X, Y, scale*Zmb, cmap=cmap)
+        ax2.pcolormesh(X, Y, scale*Zb, cmap=cmap)
         ax2.scatter(X[idx_b], Y[idx_b], s=1.5e4, c="w", edgecolors=None)
 
         for ax in (ax1, ax2):
@@ -59,6 +59,7 @@ def main(pphw=50, N=2.5, L=100, W=1, eps=0.1, sigma=0.01, plot=True):
             ax.set_ylim(Y.min(), Y.max())
 
         plt.savefig('wavefunction.jpg', bbox_inches='tight')
+        print "Wavefunction written."
 
     Z_pot = np.zeros_like(X)
 
