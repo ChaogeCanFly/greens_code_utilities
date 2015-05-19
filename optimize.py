f4c836c67b1d7f553f7870e5e82b13c536c18ad3
jdoppler <joerg.doppler@tuwien.ac.at>
2015-05-19 09:44:14 +0200

    write greens_code output to 'greens.out'

--- optimize.py
+++ optimize.py
@@ -45,7 +45,7 @@ def prepare_and_run_calc(x, N=None, L=None, W=None, pphw=None, linearized=None,
                     }
     replace_in_file(xml_template, xml, **replacements)
 
-    cmd = "mpirun -np {0} solve_xml_mumps_dev > tmp.out 2>&1".format(ncores)
+    cmd = "mpirun -np {0} solve_xml_mumps_dev > greens.out 2>&1".format(ncores)
     subprocess.call(cmd, shell=True)
 
 
